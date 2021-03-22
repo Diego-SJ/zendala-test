7338aa50baeb888434b994ec5e5b1c51b3f38180
Diego Salas <dsalas035@gmail.com>
2021-03-21 18:05:40 -0600

    env variables for firebase

--- src/core/config/firebase.ts
+++ src/core/config/firebase.ts
@@ -2,13 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+// public config
 const firebaseConfig = {
-	apiKey: 'AIzaSyCefJF9vFFj2nAcOdFGtJufz6hvSDVwWlg',
-	authDomain: 'test1-d2593.firebaseapp.com',
-	projectId: 'test1-d2593',
-	storageBucket: 'test1-d2593.appspot.com',
-	messagingSenderId: '840308951902',
-	appId: '1:840308951902:web:74d525b6341ef14861ddcf'
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 !firebase.apps.length && firebase.initializeApp(firebaseConfig);
